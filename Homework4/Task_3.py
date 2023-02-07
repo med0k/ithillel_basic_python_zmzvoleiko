0df1135ded7ed3e4b516f00d9565e22c11b2309e
med0k <zvoleikom@gmail.com>
2023-02-08 01:05:57 +0200

    third task commit for Homework4

--- Homework4/Task_3.py
+++ Homework4/Task_3.py
@@ -2,9 +2,18 @@
 Напишіть програму, яка зчитує ціле число та виводить текст, аналогічний наведеному у прикладі (пробіли та переведення
  на новий рядок важливі!). Перший рядок містить наступне значення, а другий рядок містить попереднє значення введеного числа:"""
 
-x = int(input("Please enter an integer number: "))
 
-if isinstance(x, int):
-    print('Next number for number ', x, ' is ', x+1, '.\n', 'Previous number for number ', x, ' is ', x-1, '.', sep='')
-else:
-    print("Please enter a full digit")
+def check_user_input(input):
+    try:
+        val = int(input)
+        print('Next number for number ', val, ' is ', val + 1, '.\n', 'Previous number for number ', val, ' is ', val - 1, '.',
+              sep='')
+    except ValueError:
+        try:
+            val = float(input)
+            print("Input is a float  number. Try enter an Integer value")
+        except ValueError:
+            print("No.. input is not a number. It's a string")
+
+input1 = input("Please enter an integer number: ")
+check_user_input(input1)
