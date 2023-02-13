c9b898a76913abb6158899a6b492de202e2cbbf4
med0k <zvoleikom@gmail.com>
2023-02-13 17:33:49 +0200

    Task2 commit for Homework6

--- Homework6/Task2.py
+++ Homework6/Task2.py
@@ -8,16 +8,20 @@ import math
 def solve_quadratic_equation(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
+        # a = 1, b = −8, c = 12;
+        # D = 16
         x1 = round((-b + math.sqrt(discriminant)) / 2 * a)
         x2 = round((-b - math.sqrt(discriminant)) / 2 * a)
         return x1, x2
 
     elif discriminant == 0:
+        # a = 1; b = −6; c = 9 D = 0
         x3 = (-b + math.sqrt(discriminant)) / 2 * a
         return x3
 
     else:
-        print("When discriminant = 0, do not have roots")
+        # a = 5; b = 3; c = 7; D = -131
+        print("When discriminant = 0, do not have roots, and it's ")
 
 
 def main():
