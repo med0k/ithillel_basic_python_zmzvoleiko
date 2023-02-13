b7669edebfa922304e9d10a1ae3cb184838ea295
med0k <zvoleikom@gmail.com>
2023-02-13 16:59:41 +0200

    Task2 commit for Homework6

--- Homework6/Task2.py
+++ Homework6/Task2.py
@@ -6,17 +6,29 @@ import math
 
 
 def solve_quadratic_equation(a, b, c):
-    # always returns 2(!) values: either 2 roots, 1 root and None or 2 Nones
-    x1 = - b + math.sqrt(b ** 2 - 4 * a * c) / 2 * a
-    x2 = - b - math.sqrt(b ** 2 - 4 * a * c) / 2 * a
-    return x1, x2
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant > 0:
+        x1 = round((-b + math.sqrt(discriminant)) / 2 * a)
+        x2 = round((-b - math.sqrt(discriminant)) / 2 * a)
+        return x1, x2
+
+    elif discriminant == 0:
+        x3 = (-b + math.sqrt(discriminant)) / 2 * a
+        return x3
+
+    else:
+        print("When discriminant = 0, do not have roots")
 
 
-enter_first = int(input("Please enter a: "))
-enter_second = int(input("Please enter b: "))
-enter_third = int(input("Please enter c: "))
+def main():
+    enter_first = int(input("Please enter a: "))
+    enter_second = int(input("Please enter b: "))
+    enter_third = int(input("Please enter c: "))
 
-result = solve_quadratic_equation(enter_first, enter_second, enter_third)
+    result = solve_quadratic_equation(enter_first, enter_second, enter_third)
 
-print(result)
+    print(result)
 
+
+if __name__ == '__main__':
+    main()
