8a980a97b895297a7221b3d14234544a7552e049
med0k <zvoleikom@gmail.com>
2023-02-18 17:18:00 +0200

    new test changes for Task6 commit for Homework2

--- Homework6/Task2.py
+++ Homework6/Task2.py
@@ -7,22 +7,18 @@ import math
 
 def solve_quadratic_equation(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    my_sqrt = math.sqrt(discriminant)
+    my_sqrt = math.sqrt(abs(discriminant))
     if discriminant > 0:
-        # a = 1, b = −8, c = 12;
-        # D = 16
-        x1 = (-b + my_sqrt) / 2 * a
-        x2 = (-b - my_sqrt) / 2 * a
+        x1 = (-b + my_sqrt) / (2 * a)
+        x2 = (-b - my_sqrt3) / (2 * a)
         return x1, x2
 
     elif discriminant == 0:
-        # a = 1; b = −6; c = 9 D = 0
-        x3 = -b / 2 * a
+        x3 = -b / (2 * a)
         return x3, None
 
     else:
-        # a = 5; b = 3; c = 7; D = -131
-        return None
+        return None, None
 
 
 def main():
