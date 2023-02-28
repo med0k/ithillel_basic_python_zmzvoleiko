cc7fc317e30a1395b523870ca102e2a052c3dc39
med0k <zvoleikom@gmail.com>
2023-03-01 00:20:07 +0200

    Task8 commit for Homework1

---
+++ Homework8/Task1.py
@@ -0,0 +1,24 @@
+"""
+Завдання 1.
+Дано довільний список. Створити функцію index(lst, elem), що повертатиме індекс elem в списку lst, або,
+ якщо елемент відсутній, то None. Не можна використовувати метод list().index().
+
+def index(lst, elem):  # returns integer or None
+    pass
+"""
+
+
+def index(lst, elem):  # returns integer or None
+    for num in range(0, len(lst)):
+        if lst[num] == elem:
+            return num
+    return None
+
+
+def main():
+    my_list = [5, 6, '30', '40', 'kek', 60, 56, 11, 23]
+    print(index(my_list, 23))
+
+
+if __name__ == '__main__':
+    main()
